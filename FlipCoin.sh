082e4c9a39cb91362f53f8b4f743d4ddffd1833c
rohits777 <rohitashinde95@gmail.com>
2020-09-12 14:22:43 +0530

    Added: Tripplet is added

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -37,6 +37,42 @@ function DoubletFlipper()
 		Doublet[TH]=$TH
 	fi
 }
+function TripletFlipper()
+{
+	flip=$((RANDOM%8))
+	if [ $flip -eq 0 ]
+	then
+		((HHH++))
+		Triplet[HHH]=$HHH
+	elif [ $flip -eq 1 ]
+	then
+		((HHT++))
+		Triplet[HHT]=$HHT
+	elif [ $flip -eq 2 ]
+	then
+		((HTH++))
+		Triplet[HTH]=$HTH
+	elif [ $flip -eq 3 ]
+	then
+		((HTT++))
+		Triplet[HTT]=$HTT
+	elif [ $flip -eq 4 ]
+	then
+		((THH++))
+		Triplet[THH]=$THH
+	elif [ $flip -eq 5 ]
+	then
+		((THT++))
+		Triplet[THT]=$THT
+	elif [ $flip -eq 6 ]
+	then
+		((TTH++))
+		Triplet[TTH]=$TTH
+	else
+		((TTT++))
+		Triplet[TTT]=$TTT
+	fi
+}
 
 declare -A Singlet
 H=0
@@ -78,11 +114,56 @@ do
         echo "Doublet Head-Head Percentage HH : `echo "scale=1; ${Doublet[$j]}*100/$n" | bc ` %"
     elif [[ $j -eq TT ]]
     then
-        echo "Doublet Tail-Tail Percentage TT : `echo "scale=1; ${Doublet[$j]}*100/$n" | bc ` %" 
+        echo "Doublet Tail-Tail Percentage TT : `echo "scale=1; ${Doublet[$j]}*100/$n" | bc ` %"
     elif [[ $j -eq HT ]]
     then
-        echo "Doublet Head-Tail Percentage HT : `echo "scale=1; ${Doublet[$j]}*100/$n" | bc ` %" 
+        echo "Doublet Head-Tail Percentage HT : `echo "scale=1; ${Doublet[$j]}*100/$n" | bc ` %"
     else
-        echo "Doublet Tail-Head Percentage TH : `echo "scale=1; ${Doublet[$j]}*100/$n" | bc ` %" 
+        echo "Doublet Tail-Head Percentage TH : `echo "scale=1; ${Doublet[$j]}*100/$n" | bc ` %"
+    fi
+done
+
+
+declare -A Triplet
+HHH=0
+HHT=0
+HTH=0
+HTT=0
+TTT=0
+THH=0
+THT=0
+TTH=0
+
+for (( i=0; i<$n; i++ ))
+do
+	TripletFlipper
+done
+
+for k in ${!Triplet[@]}
+do
+    echo "$k - ${Triplet[$k]}"
+    if [[ $k -eq HHH ]]
+    then
+        echo "Triplet H-H-H percentage : `echo "scale=1; ${Triplet[$k]}*100/$n" | bc ` %"
+    elif [[ $k -eq HHT ]]
+    then
+        echo "Triplet H-H-T percentage : `echo "scale=1; ${Triplet[$k]}*100/$n" | bc ` %"
+    elif [[ $k -eq HTH ]]
+    then
+        echo "Triplet H-T-H percentage : `echo "scale=1; ${Triplet[$k]}*100/$n" | bc ` %"
+    elif [[ $k -eq HTT ]]
+    then
+        echo "Triplet H-T-T percentage : `echo "scale=1; ${Triplet[$k]}*100/$n" | bc ` %"
+    elif [[ $k -eq THH ]]
+    then
+        echo "Triplet T-H-H percentage : `echo "scale=1; ${Triplet[$k]}*100/$n" | bc ` %"
+    elif [[ $k -eq THT ]]
+    then
+        echo "Triplet T-H-T percentage : `echo "scale=1; ${Triplet[$k]}*100/$n" | bc ` %"
+    elif [[ $k -eq TTH ]]
+    then
+        echo "Triplet T-T-H percentage : `echo "scale=1; ${Triplet[$k]}*100/$n" | bc ` %"
+    else
+        echo "Triplet T-T-T percentage : `echo "scale=1; ${Triplet[$k]}*100/$n" | bc ` %"
     fi
 done
